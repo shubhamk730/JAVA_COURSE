8a0b80c89bb3c988fd9cb2d78c5b5470f55f6415
shubham <skaushik241000@gmail.com>
2023-02-07 18:34:06 +0530

    Java variables and data types

---
+++ Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        // integer variable
+          int num1 = 5;
+          int num2 = 9;
+          System.out.println(num1 + num2);
+
+          long l1 = 76555l;
+          System.out.println(l1);
+
+        // Data Types
+        // primitive -> Integer(byte, short, int, long), float(double, float), character, boolean
+        // Non primitive
+        // float f1 = 5.7; // gives error
+        float f1 = 5.7f;
+        System.out.println(f1);
+
+        char c = 'a';
+        System.out.println(c);
+
+        boolean b = false;
+        System.out.println(b);
+
+    }
+}
