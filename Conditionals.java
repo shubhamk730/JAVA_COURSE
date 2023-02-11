fca954dfeff272783daf4faee38d5a52ea8eac67
shubham <skaushik241000@gmail.com>
2023-02-11 13:05:21 +0530

    Print week days conditionally

--- Conditionals.java
+++ Conditionals.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Conditionals {
     public static void main(String[] args) {
         int a = 8, b = 9;
@@ -17,13 +19,34 @@ public class Conditionals {
         // Switchh statement
         int day = 1;
         //  1 -> monday, ... , 7 -> Sunday
-        switch(day) {
-            case 1: System.out.println("Monday");
-                    break;
-            case 2: System.out.println("Tuesday");
-                    break;
-            default: System.exit(0);
+        Scanner scn = new Scanner(System.in);
+        while(day <= 10) {
+            System.out.print("Enter the day : ");
+            day = scn.nextInt();
+            System.out.println();
+            switch(day) {
+                case 1: System.out.println("Monday");
+                        break;
+                case 2: System.out.println("Tuesday");
+                        break;
+                case 3: System.out.println("wednesday");
+                        break;
+                case 4: System.out.println("Thursday");
+                        break;
+                case 5: System.out.println("Friday");
+                        break;
+                case 6: System.out.println("Saturday");
+                        break;
+                case 7: System.out.println("Sunday");
+                        break;
+                default: System.out.println("Invalid");
+                        // System.exit(0);
+                        break;
+            }
+            System.out.println("Stil inside");
         }
+        
         System.out.println("out now");
+        scn.close();
     }
 }
