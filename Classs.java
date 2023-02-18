2527330011a68c7fb20565d3c94677ffc25ecedf
shubham <skaushik241000@gmail.com>
2023-02-18 20:39:22 +0530

    Loops and OOPS basics

---
+++ Classs.java
@@ -0,0 +1,11 @@
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+public class Classs {
+    public static void main(String[] args) {
+        Calculator o = new Calculator();
+        System.out.println(o.add(2,30));
+    }
+}
---
+++ Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        while(a < 10) {
+            System.out.println("Hello");
+            a++;
+        }
+
+        do{
+            System.out.println("Hello again");
+        }while(a<10);
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println("For loop");
+        }
+
+    }
+
+}
